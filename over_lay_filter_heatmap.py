d9a6c7f736ccc55dade8bc6432e126a59ffa7aa7
yukikawana <kojirou.tensou@gmail.com>
2017-11-08 15:39:50 +0000

    updated

--- over_lay_filter_heatmap.py
+++ over_lay_filter_heatmap.py
@@ -27,6 +27,7 @@ input_shape = (360, 480)
 inputs = tf.placeholder("float", shape=(1, input_shape[0], input_shape[1], 3))
 logits, end_points = segnet(inputs)
 print logits
+prob = tf.nn.softmax(logits)
 outputs = tf.argmax(logits)
 sess = tf.Session()
 saver = tf.train.Saver()
@@ -42,8 +43,10 @@ name = 'conv5_3'
 name = 'relu5_3'
 name = 'relu3_3'
 name = 'conv1_1_D'
-name = 'relu5_3'
-cvimg = cv2.resize(cv2.imread('car2.png'), (input_shape[1], input_shape[0]))
+name = 'relu1_2_D'
+name = 'relu4_3'
+name = 'relu5_2'
+cvimg = cv2.resize(cv2.imread('car.png'), (input_shape[1], input_shape[0]))
 nimage = cvimg
 nimage = np.expand_dims(nimage,0 )
 obj = 9
@@ -61,6 +64,7 @@ signal = tf.multiply(logits, masks)
 print logits
 print masks.shape
 loss = tf.reduce_mean(signal)
+#loss = tf.reduce_sum((prob-masks)**2)
 grads = tf.gradients(loss, end_points[name])[0]
 # Normalizing the gradients
 norm_grads = tf.div(grads, tf.sqrt(tf.reduce_mean(tf.square(grads))) + tf.constant(1e-5))
@@ -71,23 +75,30 @@ layer = np.squeeze(layer)
 grads_val = sess.run([norm_grads], feed_dict={inputs: nimage})
 grads_val = grads_val[0]
 weights = np.mean(grads_val, axis = (0, 1, 2)) 			# [512]
+print weights.shape
+weights_sorted = np.sort(weights)[::-1]
+percentage_pre = (weights_sorted - np.min(weights_sorted))/np.ptp(weights_sorted)
+percentage = percentage_pre/np.sum(percentage_pre)
+print percentage
+#plt.plot(np.cumsum(percentage))
+plt.plot(percentage)
+plt.savefig("savefig.png")
+
 idxs = np.argsort(weights)[::-1]
-idx = idxs[10]
+idx = idxs[3]
 layer = np.asarray(layer)
-print layer.shape
 channel = np.squeeze(layer[:,:,idx])
 channel = (channel - np.min(channel, axis=(0, 1)))/np.ptp(channel)*255
 cam_heatmap = cv2.resize(cv2.applyColorMap(np.uint8(channel), cv2.COLORMAP_JET), (input_shape[1], input_shape[0]))
-print cam_heatmap.shape
-print cvimg.shape
 cc = cam_heatmap*0.5+cvimg+0.5
 cc = np.float32(cc)/np.max(cc,axis=(0,1,2))*255
 cv2.imwrite('{}_{}_heatmap.png'.format(name,idx),np.uint8(cc))
+print idx, name
+
 def get_img(idx, name):
-    #img = visualize_activation_tf(inputs,end_points,name, filter_indices=idx, tv_weight=.0,lp_norm_weight=10., input_modifiers=[Jitter(0.05)],max_iter=500, verbose=True)
-    #img = visualize_activation_tf(inputs,end_points,name, filter_indices=idx,seed_input=img, max_iter=600, act_max_weight=1.,tv_weight=0.01,lp_norm_weight=10, input_modifiers=[Jitter(16)], verbose=True)
-    img = visualize_activation_tf(inputs,end_points,name, filter_indices=idx, max_iter=600, act_max_weight=1.,tv_weight=0.01,lp_norm_weight=10, input_modifiers=[Jitter(16)], verbose=True)
-#    img = visualize_activation_tf(inputs,end_points,name, filter_indices=idx, max_iter=1800, act_max_weight=1.,tv_weight=0.01,lp_norm_weight=10000, input_modifiers=[Jitter(16)], verbose=True)
+    #img = visualize_activation_tf(inputs,end_points,name, filter_indices=idx, max_iter=600, act_max_weight=1.,tv_weight=0.01,lp_norm_weight=10, input_modifiers=[Jitter(16)], verbose=True)
+    seed = visualize_activation_tf(inputs, end_points, name, filter_indices=idx,tv_weight=0.,act_max_weight=100., input_modifiers=[Jitter(0.05)],verbose=True)
+    img = visualize_activation_tf(inputs, end_points, name, filter_indices=idx,seed_input=seed,tv_weight=5., act_max_weight=15.,input_modifiers=[Jitter(0.05)], verbose=True)
     return img
 img = get_img(idx, name)
 Image.fromarray(np.uint8(img)).save('{}_{}.png'.format(name,idx))
